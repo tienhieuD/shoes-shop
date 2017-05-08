4c16eed8ec89cb6cfc5181ae6338d4441d7e7dfe
dNG1216 <duongnguyen1216@gmail.com>
2017-05-08 20:54:12 +0700

    XXX

---
+++ csdl/csdl.php
@@ -0,0 +1,18 @@
+﻿<?php
+	$con=mysqli_connect("localhost","root","");
+	if (mysqli_connect_errno($con)){
+		echo "<br>Không thể kết nối đến CSDL: " . mysqli_connect_error($con);
+		echo "<br>Không thể kết nối đến CSDL: Mã lỗi là " . mysqli_connect_errno($con);
+	}
+	$sql="CREATE DATABASE IF NOT EXISTS `qlbangiay` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;";
+	if (mysqli_query($con,$sql)){
+		echo "CSDL đã được tạo";
+		echo "<br/><a href='taobang.php'>Bấm đây để tạo bảng và dữ liệu</a>";
+	}else{
+		echo "<br>Lỗi khi tạo CSDL: " . mysqli_connect_error($con);
+		echo "<br>Lỗi khi tạo CSDL: Mã lỗi là " . mysqli_connect_errno($con);
+	}
+	
+	
+?>
+

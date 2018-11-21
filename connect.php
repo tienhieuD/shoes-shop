d4fd0271f4b33786946940e269c44a3fb7d44327
tienhieud <tienhieud@github.com>
2018-11-21 14:11:42 +0700

    Update Server Info

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 ﻿<?php 
 	
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "qlbangiay";
+	$servername = "duongnguyen1216@gmail.com";
+	$username = "sql12266394";
+	$password = "umTXf9YMQ3";
+	$dbname = "sql12266394";
 	
 	$conn = mysqli_connect($servername, $username, $password, $dbname, 3306);
 	if ($conn->connect_error) {
